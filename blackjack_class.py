8ed819f42e9b70bd7db4b9e86d1a5e58375e713c
Preeti Sehgal <psehgal@zipcodes-mbp-4.lan>
2020-03-28 15:28:25 -0400

    created a new branch final code as i am not sure if pull request was successfull for my previus branch, in this code i have added more functions and test cases for dealing the deck to the player and dealer and counting their points

--- blackjack_class.py
+++ blackjack_class.py
@@ -7,8 +7,9 @@ from random import shuffle
 #All classes will be part of this module and game willl be a separate .py program file
 
 class Deck:
+
     def __init__(self):
-        self = self
+        pass
 
 # A function for creating a deck
 # The deck will contain cards of Suits: Heart, Spade, Diamond, Clubs  and Ranks: Ace, 2-10, Jack, Queen, King
@@ -26,19 +27,52 @@ class Deck:
         return result
 
 
+class Deal:
 
-
-
-
+    def __init__(self):
+        pass
 
 # A function for creating player and dealers hands
-#The player and dealer will get 2 cards initially
+# The player and dealer will get 2 cards initially
+
+    def deal_cards(self,cards):
+        player_hand = []
+        dealer_hand = []
+
+        player_hand.append(cards.pop())
+        player_hand.append(cards.pop())
+        dealer_hand.append(cards.pop())
+        dealer_hand.append(cards.pop())
+
+        return [player_hand, dealer_hand]
 
 
+class Calculation:
+
+    def __init__(self):
+        pass
 
 # A function for counting the points
 # This function will count the points of the player and the dealer
 # The point calculation logic will also determine the value of ACE as 1 or 11 depending on the situation
+
+    def point_calc(self, my_cards):
+        point = 0
+        ace = 0
+        for i in my_cards:
+            if i[1] in ['J','Q','K']:
+                point = point + 10
+            elif i[1] in ['1','2','3','4','5','6','7','8','9','10']:
+                point = point + int(i[1])
+            else:
+                point = point + 1
+                ace = 1
+        if ace == 1 and point <= 10:
+            point = point + 10
+
+
+        return point
+
 
 if __name__ == "__main__":
     mydeck = Deck()
--- test_class.py
+++ test_class.py
@@ -1,13 +1,42 @@
 import unittest
 import blackjack_class
 
+
 class TestDeck(unittest.TestCase):
     def test_deckcards(self):
         expected_list = [
-            'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'Hk',
-            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'Sk',
-            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'Dk',
-            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'Ck',
+            'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK',
+            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK',
+            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK',
+            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'CK',
             ]
         testing_class = blackjack_class.Deck()
         self.assertEqual(testing_class.deck_cards().sort(), expected_list.sort())
+
+    def test_dealcards(self):
+        deck = [
+            'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK',
+            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK',
+            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK',
+            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'CK',
+            ]
+
+        test_class = blackjack_class.Deal()
+        num_cards = test_class.deal_cards(deck)
+        self.assertEqual(len(num_cards), 2)
+
+    def test_pointcalc(self):
+        lst = ['S7', 'HA', "DJ"]
+        test_class = blackjack_class.Calculation()
+        total_points = test_class.point_calc(lst)
+        self.assertEqual(total_points, 18)
+
+        lst = ['SA', 'HA', "DJ"]
+        test_class = blackjack_class.Calculation()
+        total_points = test_class.point_calc(lst)
+        self.assertEqual(total_points, 12)
+
+        lst = ['S1', 'HA', "D5"]
+        test_class = blackjack_class.Calculation()
+        total_points = test_class.point_calc(lst)
+        self.assertEqual(total_points, 17)
