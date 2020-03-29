d79fca3500fd777c519a476782a7c58f1bd7e94d
Preeti Sehgal <psehgal@zipcodes-mbp-4.lan>
2020-03-29 01:02:34 -0400

    created the blackjack game by calling the classes and functions from blackjack.py and also created the unit test file test_game.py and executed the test case, played the game multiple times to check the functionality and everything seems to be working as expected

--- blackjack_class.py
+++ blackjack_class.py
@@ -1,27 +1,27 @@
-
 from random import shuffle
 
-#Create classes for blackjack program
-#This .py file will work as a module of classes
-#That will be imported in the main program
-#All classes will be part of this module and game willl be a separate .py program file
+
+# Create classes for blackjack program
+# This .py file will work as a module of classes
+# That will be imported in the main program
+# All classes will be part of this module and game willl be a separate .py program file
 
 class Deck:
 
     def __init__(self):
         pass
 
-# A function for creating a deck
-# The deck will contain cards of Suits: Heart, Spade, Diamond, Clubs  and Ranks: Ace, 2-10, Jack, Queen, King
+    # A function for creating a deck
+    # The deck will contain cards of Suits: Heart, Spade, Diamond, Clubs  and Ranks: Ace, 2-10, Jack, Queen, King
     def deck_cards(self):
         result = []
         suit = ['H', 'S', 'D', 'C']
-        num = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        num = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
         for i in suit:
             for n in num:
                 result.append(i + n)
 
-# The function will return a shuffled deck with 52 cards
+        # The function will return a shuffled deck with 52 cards
         shuffle(result)
 
         return result
@@ -32,10 +32,10 @@ class Deal:
     def __init__(self):
         pass
 
-# A function for creating player and dealers hands
-# The player and dealer will get 2 cards initially
+    # A function for creating player and dealers hands
+    # The player and dealer will get 2 cards initially
 
-    def deal_cards(self,cards):
+    def deal_cards(self, cards):
         player_hand = []
         dealer_hand = []
 
@@ -52,24 +52,26 @@ class Calculation:
     def __init__(self):
         pass
 
-# A function for counting the points
-# This function will count the points of the player and the dealer
-# The point calculation logic will also determine the value of ACE as 1 or 11 depending on the situation
+    # A function for counting the points
+    # This function will count the points of the player and the dealer
+    # The point calculation logic will also determine the value of ACE as 1 or 11 depending on the situation
 
     def point_calc(self, my_cards):
         point = 0
         ace = 0
         for i in my_cards:
-            if i[1] in ['J','Q','K']:
+            if i[1] in ['T', 'J', 'Q', 'K']:
                 point = point + 10
-            elif i[1] in ['1','2','3','4','5','6','7','8','9','10']:
+            elif i[1] in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
                 point = point + int(i[1])
             else:
-                point = point + 1
-                ace = 1
-        if ace == 1 and point <= 10:
-            point = point + 10
+                point = point + 11
+                ace = ace + 1
 
+        if ace > 0:
+            for i in (1, ace + 1):
+                if point > 21:
+                    point = point - 10
 
         return point
 
@@ -79,11 +81,12 @@ class Player:
     def __init__(self):
         pass
 
-    def player_hit(self,my_cards,deck):
+    def player_hit(self, my_cards, deck):
         my_cards.append(deck.pop(0))
         calc_class = Calculation()
         points = calc_class.point_calc(my_cards)
-        return (my_cards,deck,points)
+        return (my_cards, deck, points)
+
 
 class Dealer(Player):
 
@@ -101,11 +104,6 @@ class Dealer(Player):
         return results
 
 
-
-
-
-
 if __name__ == "__main__":
     mydeck = Deck()
     print(mydeck.deck_cards())
-
--- test_class.py
+++ test_class.py
@@ -5,20 +5,20 @@ import blackjack_class
 class TestDeck(unittest.TestCase):
     def test_deckcards(self):
         expected_list = [
-            'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK',
-            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK',
-            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK',
-            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'CK',
+            'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'HT', 'HJ', 'HQ', 'HK',
+            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'ST', 'SJ', 'SQ', 'SK',
+            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'DT', 'DJ', 'DQ', 'DK',
+            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'CT', 'CJ', 'CQ', 'CK',
             ]
         testing_class = blackjack_class.Deck()
         self.assertEqual(testing_class.deck_cards().sort(), expected_list.sort())
 
     def test_dealcards(self):
         deck = [
-            'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK',
-            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK',
-            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK',
-            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'CK',
+            'HA', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'HT', 'HJ', 'HQ', 'HK',
+            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'ST', 'SJ', 'SQ', 'SK',
+            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'DT', 'DJ', 'DQ', 'DK',
+            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'CT', 'CJ', 'CQ', 'CK',
             ]
 
         test_class = blackjack_class.Deal()
@@ -26,27 +26,27 @@ class TestDeck(unittest.TestCase):
         self.assertEqual(len(num_cards), 2)
 
     def test_pointcalc(self):
-        lst = ['S7', 'HA', "DJ"]
+        lst = ['S4', 'DT']
         test_class = blackjack_class.Calculation()
         total_points = test_class.point_calc(lst)
-        self.assertEqual(total_points, 18)
+        self.assertEqual(total_points, 14)
 
-        lst = ['SA', 'HA', "DJ"]
+        lst = ['HA', "DA", 'HJ']
         test_class = blackjack_class.Calculation()
         total_points = test_class.point_calc(lst)
         self.assertEqual(total_points, 12)
 
-        lst = ['S1', 'HA', "D5"]
+        lst = ['SA', 'HA', "DA"]
         test_class = blackjack_class.Calculation()
         total_points = test_class.point_calc(lst)
-        self.assertEqual(total_points, 17)
+        self.assertEqual(total_points, 13)
 
     def test_playerhit(self):
         deck = [
-            'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK',
-            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK',
-            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK',
-            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'CK',
+            'H5', 'H6', 'H7', 'H8', 'H9', 'HT', 'HJ', 'HQ', 'HK',
+            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'ST', 'SJ', 'SQ', 'SK',
+            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'DT', 'DJ', 'DQ', 'DK',
+            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'CT', 'CJ', 'CQ', 'CK',
         ]
 
         player_deck = ['HA', 'H2', ]
@@ -59,10 +59,10 @@ class TestDeck(unittest.TestCase):
 
     def test_dealerhit(self):
         deck = [
-            'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK',
-            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK',
-            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK',
-            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'CK',
+            'H5', 'H6', 'H7', 'H8', 'H9', 'HT', 'HJ', 'HQ', 'HK',
+            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'ST', 'SJ', 'SQ', 'SK',
+            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'DT', 'DJ', 'DQ', 'DK',
+            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'CT', 'CJ', 'CQ', 'CK',
         ]
 
         dealer_deck = ['HA', 'H2', ]
@@ -75,9 +75,9 @@ class TestDeck(unittest.TestCase):
 
         deck = [
             'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'HJ', 'HQ', 'HK',
-            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK',
-            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK',
-            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'CJ', 'CQ', 'CK',
+            'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'ST', 'SJ', 'SQ', 'SK',
+            'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'DT', 'DJ', 'DQ', 'DK',
+            'CA', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'CT', 'CJ', 'CQ', 'CK',
         ]
         result =[]
         dealer_deck = ['HJ', 'HK', ]
